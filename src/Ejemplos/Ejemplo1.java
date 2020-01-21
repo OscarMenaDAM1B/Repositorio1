ceeec8fdf8fbdd5d0632bb096b590183344b7bfc
1b_dam20_15 <1b_dam20_15@A22_PC04.JOVELLANOS.EDU>
2020-01-21 20:20:14 +0100

    tytryrtyr
    Signed-off-by: 1b_dam20_15 <1b_dam20_15@A22_PC04.JOVELLANOS.EDU>

---
+++ src/Ejemplos/Ejemplo1.java
@@ -0,0 +1,14 @@
+package Ejemplos;
+
+public class Ejemplo1 {
+	public static void main(String[] args) {
+		
+		int a = 2;
+		int b = 3;
+		
+		int suma = a + b;
+		System.out.println("El resultado es:  " + suma);
+				
+	}
+
+}
---
+++ src/Ejemplos/Ejemplo2.java
@@ -0,0 +1,14 @@
+package Ejemplos;
+
+public class Ejemplo2 {
+	public static void main(String[] args) {
+		
+		int c = 2;
+		int b = 4;
+		
+		int suma = c + b;
+		System.out.println("El resultado es:  " + suma);
+	
+	}
+
+}
